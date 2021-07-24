759b53a734caa93f1f93e1fb01e65454c125e6cc
@uuuuujin63 <41807132+uuuuujin63@users.noreply.github.com>
2021-07-24 23:06:09 +0900

    Create 상수.java

---
+++ 2week/상수.java
@@ -0,0 +1,18 @@
+//2908번
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException   {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int a = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+		int b = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+		
+		System.out.println(a>b ? a:b);
+	}
+}
