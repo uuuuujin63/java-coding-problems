e5452c9d6bda3c868a96f1bad11c61f9e5bea7bc
@uuuuujin63 <41807132+uuuuujin63@users.noreply.github.com>
2021-07-24 15:22:16 +0900

    Create 숫자의합.java

---
+++ 2week/숫자의합.java
@@ -0,0 +1,20 @@
+//11720번
+
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException   {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		br.readLine();
+		
+		int sum = 0;
+		
+		
+		for(byte value : br.readLine().getBytes()) {
+			sum += (value - '0');
+		}
+		System.out.println(sum);
+	}
+}
