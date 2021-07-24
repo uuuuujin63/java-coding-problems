db680f28e14cd5d06bddb46c1b8e331d3776e4c5
@uuuuujin63 <41807132+uuuuujin63@users.noreply.github.com>
2021-07-24 15:02:09 +0900

    Create 아스키코드.java

---
+++ 2week/아스키코드.java
@@ -0,0 +1,12 @@
+//11654번
+
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException   {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		char[] a = br.readLine().toCharArray();
+		System.out.println((int)a[0]);
+	}
+}
