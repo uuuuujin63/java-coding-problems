c20018f631c79f3a4a20c5c6807f84e526e4d320
@uuuuujin63 <41807132+uuuuujin63@users.noreply.github.com>
2021-08-04 17:41:22 +0900

    Update 균형잡힌세상.java

--- Study_1week/균형잡힌세상.java
+++ Study_1week/균형잡힌세상.java
@@ -1,1 +1,43 @@
 
+import java.io.*;
+import java.util.Stack;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		while (true) {
+			char[] line = br.readLine().toCharArray();
+			int n = line.length;
+			if (n == 1 && line[0] == '.')
+				break;
+			Stack<Character> stack = new Stack<>();
+
+			int flg = 1;
+			for (int i = 0; i < n; i++) {
+				if (line[i] == '(' || line[i] == '[') {
+					stack.push(line[i]);
+				} else if (line[i] == ']' || line[i] == ')') {
+					if (stack.empty()) {
+						flg = 0;
+						break;
+					}
+					char pop_stack = stack.pop();
+					if (line[i] == ']' && pop_stack!='[') {
+						flg = 0;
+						break;
+					} else if (line[i] == ')' && pop_stack!='(') {
+						flg = 0;
+						break;
+					}
+				}
+			}
+			if (stack.empty() && flg == 1)
+				sb.append("yes\n");
+			else
+				sb.append("no\n");
+		}
+		System.out.println(sb);
+	}
+}
