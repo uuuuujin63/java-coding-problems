dca80a80f7cae20d9610c4c4ab963ab3c32a503c
@uuuuujin63 <41807132+uuuuujin63@users.noreply.github.com>
2022-02-28 09:17:06 +0900

    Update 코드배틀.java

--- 202202/코드배틀.java
+++ 202202/코드배틀.java
@@ -1,69 +1,136 @@
-/////////////////////////////////////////////////////////////////////////////////////////////
-// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
-// 아래 표준 입출력 예제 필요시 참고하세요.
-// 표준 입력 예제
-// int a;
-// double b;
-// char g;
-// String var;
-// long AB;
-// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
-// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
-// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
-// var = sc.next();                            // 문자열 1개 입력받는 예제
-// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
-/////////////////////////////////////////////////////////////////////////////////////////////
-// 표준 출력 예제
-// int a = 0;                            
-// double b = 1.0;               
-// char g = 'b';
-// String var = "ABCDEFG";
-// long AB = 12345678901234567L;
-//System.out.println(a);                       // int 변수 1개 출력하는 예제
-//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
-//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
-//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
-//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
-/////////////////////////////////////////////////////////////////////////////////////////////
-import java.util.Scanner;
-import java.io.FileInputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-/*
-   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
-   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
- */
-class Solution
-{
-	public static void main(String args[]) throws Exception
-	{
-		/*
-		   아래의 메소드 호출은 앞으로 표준 입력(키보드) 대신 input.txt 파일로부터 읽어오겠다는 의미의 코드입니다.
-		   여러분이 작성한 코드를 테스트 할 때, 편의를 위해서 input.txt에 입력을 저장한 후,
-		   이 코드를 프로그램의 처음 부분에 추가하면 이후 입력을 수행할 때 표준 입력 대신 파일로부터 입력을 받아올 수 있습니다.
-		   따라서 테스트를 수행할 때에는 아래 주석을 지우고 이 메소드를 사용하셔도 좋습니다.
-		   단, 채점을 위해 코드를 제출하실 때에는 반드시 이 메소드를 지우거나 주석 처리 하셔야 합니다.
-		 */
-		//System.setIn(new FileInputStream("res/input.txt"));
+class Solution {
+	private final static int CMD_INIT = 100;
+	private final static int CMD_INSERT = 200;
+	private final static int CMD_MOVECURSOR = 300;
+	private final static int CMD_COUNT = 400;
 
-		/*
-		   표준입력 System.in 으로부터 스캐너를 만들어 데이터를 읽어옵니다.
-		 */
-		Scanner sc = new Scanner(System.in);
-		int T;
-		T=sc.nextInt();
-		/*
-		   여러 개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
-		*/
+	private final static UserSolution usersolution = new UserSolution();
 
-		for(int test_case = 1; test_case <= T; test_case++)
-		{
-		
-			/////////////////////////////////////////////////////////////////////////////////////////////
-			/*
-				 이 부분에 여러분의 알고리즘 구현이 들어갑니다.
-			 */
-			/////////////////////////////////////////////////////////////////////////////////////////////
+	private static void String2Char(char[] buf, String str, int maxLen) {
+		for (int k = 0; k < str.length(); k++)
+			buf[k] = str.charAt(k);
 
+		for (int k = str.length(); k <= maxLen; k++)
+			buf[k] = '\0';
+	}
+
+	private static char[] mStr = new char[90001];
+
+	private static boolean run(BufferedReader br) throws Exception {
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+		int queryCnt = Integer.parseInt(st.nextToken());
+		boolean correct = false;
+
+		for (int q = 0; q < queryCnt; q++) {
+			st = new StringTokenizer(br.readLine(), " ");
+
+			int cmd = Integer.parseInt(st.nextToken());
+
+			if (cmd == CMD_INIT) {
+				int H = Integer.parseInt(st.nextToken());
+				int W = Integer.parseInt(st.nextToken());
+
+				String2Char(mStr, st.nextToken(), 90000);
+
+				usersolution.init(H, W, mStr);
+				correct = true;
+			} else if (cmd == CMD_INSERT) {
+				char mChar = st.nextToken().charAt(0);
+
+				usersolution.insert(mChar);
+			} else if (cmd == CMD_MOVECURSOR) {
+				int mRow = Integer.parseInt(st.nextToken());
+				int mCol = Integer.parseInt(st.nextToken());
+
+				char ret = usersolution.moveCursor(mRow, mCol);
+
+				char ans = st.nextToken().charAt(0);
+				if (ret != ans) {
+					correct = false;
+				}
+			} else if (cmd == CMD_COUNT) {
+				char mChar = st.nextToken().charAt(0);
+
+				int ret = usersolution.countCharacter(mChar);
+
+				int ans = Integer.parseInt(st.nextToken());
+				if (ret != ans) {
+					correct = false;
+				}
+			}
 		}
+		return correct;
+	}
+
+	public static void main(String[] args) throws Exception {
+		int TC, MARK;
+		long tmp, tc_time_cost, total_time_cost = 0;
+
+		// System.setIn(new java.io.FileInputStream("res/sample_input.txt"));
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+		TC = Integer.parseInt(st.nextToken());
+		MARK = Integer.parseInt(st.nextToken());
+
+		for (int testcase = 1; testcase <= TC; ++testcase) {
+			tmp = System.nanoTime();
+			int score = run(br) ? MARK : 0;
+			total_time_cost += System.nanoTime() - tmp;
+			System.out.println("#" + testcase + " " + score);
+		}
+		System.out.println("#TIME " + total_time_cost / 1000);
+		br.close();
+	}
+}
+
+class UserSolution {
+	static StringBuilder str;
+	static int w, h, len, cursor;
+
+	void init(int H, int W, char mStr[]) {
+		str = new StringBuilder();
+		for (int i = 0; i < 90001; i++) {
+			if (mStr[i] == '\0')
+				break;
+			str.append(mStr[i]);
+		}
+		cursor = 0;
+		w = W;
+		h = H;
+	}
+
+	void insert(char mChar) {
+		str.insert(cursor, mChar);
+		cursor++;
+	}
+
+	char moveCursor(int mRow, int mCol) {
+		len = str.length();
+		int now = (mRow - 1) * w + (mCol - 1);
+		if (now >= len) {
+			cursor = len;
+			return '$';
+		} else {
+			cursor = now;
+			return str.charAt(now);
+		}
+	}
+
+	int countCharacter(char mChar) {
+		len = str.length();
+		int cnt = 0;
+		for (int i = cursor; i < len; i++) {
+			if (str.charAt(i) == mChar) {
+				cnt++;
+			}
+		}
+		return cnt;
 	}
 }
